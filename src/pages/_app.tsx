1d7c65f6069fa0320f07aabb0e182f8ff7cb5aa3
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-06-25 13:16:42 -0300

    Configuration Chakra Ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
-import { AppProps } from 'next/app'
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react';
+import { AppProps } from 'next/app';
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp
