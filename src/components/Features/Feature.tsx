77a477eafb94e572c781b9bbd4e7dd6cc8be392c
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-06-25 23:05:00 -0300

    Update index.tsx

--- src/components/Features/Feature.tsx
+++ src/components/Features/Feature.tsx
@@ -6,14 +6,14 @@ interface FeatureProps {
 }
 
 export function Feature({ icon, text }: FeatureProps) {
-  const isMobile = useBreakpointValue({
+  const isWideScreen = useBreakpointValue({
     base: false,
     sm: true
   })
 
   return (
     <Flex direction={["row", "column"]} align="center" justify="center">
-      {isMobile ? <Image src={`/icons/${icon}.svg`} w="85px" h="85px" mb="6" /> : <Text color="yellow.400" fontSize="4xl" mr="2">•</Text>}
+      {isWideScreen ? <Image src={`/icons/${icon}.svg`} w="85px" h="85px" mb="6" /> : <Text color="yellow.400" fontSize="4xl" mr="2">•</Text>}
       <Text fontWeight="600" color="gray.700" fontSize={["md", "xl", "2xl"]}>{text}</Text>
     </Flex>
   )
